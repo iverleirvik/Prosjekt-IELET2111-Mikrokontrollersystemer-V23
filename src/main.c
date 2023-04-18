74a01e7da5fe74dc114c4e7b177362ffbeaa32ba
iverleirvik <94975465+iverleirvik@users.noreply.github.com>
2023-04-18 13:08:13 +0200

    Ednret Main igjen.

--- src/main.c
+++ src/main.c
@@ -2,11 +2,6 @@
 
 #include <avr/io.h>
 
-
-void setup() {
-  // put your setup code here, to run once:
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
+int main(void)  {
+  
 }
