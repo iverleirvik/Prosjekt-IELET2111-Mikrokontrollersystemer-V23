b79d7850482cca8d154d07c28cd0addbc4fb00ff
iverleirvik <94975465+iverleirvik@users.noreply.github.com>
2023-04-19 13:52:09 +0200

    Update RTC.h

--- lib/RTC/RTC.h
+++ lib/RTC/RTC.h
@@ -1,1 +1,27 @@
+#ifndef RTC_h
+#define RTC_h
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+RTC_init(void) {
+
+  RTC.CTRLA |= RTC_RTCEN_bm | RTC_PRESCALER_DIV1024_gc;
+	RTC.INTCTRL |= RTC_OVF_bm;
+	RTC.CLKSEL |= RTC_CLKSEL_OSC1K_gc;
+	RTC.PER |= 1;
+
+}
+
+ISR(RTC_CNT_vect)	{
+	if(RTC.INTFLAGS & RTC_OVF_bm)	{
+			/* Kjør kode */
+			//secons ++;
+			//TCA0.SINGLE.CNT	= 0;
+	}
+		
+	RTC.INTFLAGS |= RTC_OVF_bm;
+}
+
+
+#endif
