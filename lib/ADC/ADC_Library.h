e2ef2324368551486ef9a0efabec2321d3211f4a
erlebors <94974987+erlebors@users.noreply.github.com>
2023-04-20 15:29:28 +0200

    Oppdaterer ADC lib

--- lib/ADC/ADC_Library.h
+++ lib/ADC/ADC_Library.h
@@ -1,4 +1,16 @@
+#ifndef ADC_h
+#define ADC_h
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
 #include <stdint.h>
+#include <math.h>
 
 void ADC0_init(char channel);
 uint16_t ADC0_read(void);
+float adcRun(int state);
+float spenningEkstern(uint8_t adcVal);
+float spenningMCU(uint8_t adcVal);
+float temp(float adcVal);
+
+#endif
